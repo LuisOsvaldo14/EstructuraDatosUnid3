36b6447ef96a576bc4f5ac3da4d802eb041f336a
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:37:12 -0400

    ejercicio 7

---
+++ APLI7/APLI7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class BuscarLista
+{
+    static void Main()
+    {
+        int[] numeros = { 10, 25, 30, 45, 50 };
+        Console.WriteLine("Lista disponible: " + string.Join(", ", numeros));
+        Console.Write("¿Qué número buscas?: ");
+        int buscar = int.Parse(Console.ReadLine());
+
+        if (Existe(buscar, numeros)) Console.WriteLine("¡Encontrado!");
+        else Console.WriteLine("No está en la lista.");
+    }
+
+    static bool Existe(int n, int[] lista) => lista.Contains(n);
+}
