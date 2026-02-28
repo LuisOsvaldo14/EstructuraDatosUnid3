5b7fb04eb7566aff1b6c0740c89b7970694df85a
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 23:33:44 -0400

    ejercicio17

---
+++ Ejercicio 17/Ejercicio 17/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections.Generic;
+
+var ciudades = ("Santo Domingo", "Madrid", "París", "Tokio", "Roma");
+var listaCiudades = new List<string> { ciudades.Item1, ciudades.Item2, ciudades.Item3, ciudades.Item4, ciudades.Item5 };
+Console.WriteLine($"Primer ciudad de la lista: {listaCiudades[0]}");
