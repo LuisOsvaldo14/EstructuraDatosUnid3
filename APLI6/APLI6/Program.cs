5cac8e092d8ba86f2f2eb50b8ddad464f17d766a
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:32:19 -0400

    ejercicio6

---
+++ APLI6/APLI6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Conversor
+{
+    static void Main()
+    {
+        Console.Write("Grados Celsius: ");
+        double c = double.Parse(Console.ReadLine());
+        Console.WriteLine($"{c}°C equivalen a {ConvertirAFahrenheit(c)}°F");
+    }
+
+    static double ConvertirAFahrenheit(double celsius) => (celsius * 1.8) + 32;
+}
