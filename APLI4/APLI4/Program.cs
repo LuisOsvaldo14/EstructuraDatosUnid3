81d69817168b2401fda50bd4e1c8d6e145a11b8a
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:22:44 -0400

    APLI5

---
+++ APLI4/APLI4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class NumeroMayor
+{
+    static void Main()
+    {
+        Console.Write("Primer número: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.Write("Segundo número: ");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"El mayor es: {ObtenerMayor(x, y)}");
+    }
+
+    static int ObtenerMayor(int a, int b) => (a > b) ? a : b;
+}
