08de775fdb74a0f5de3eddc0d6f27963705d8d01
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 23:39:32 -0400

    Ejercicio 18

---
+++ Ejercicio18/Ejercicio18/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Linq;
+
+var numsRepetidos = new int[] { 1, 2, 2, 3, 2, 4 }; 
+int buscar = 2;
+int conteo = numsRepetidos.Count(x => x == buscar);
+Console.WriteLine($"El número {buscar} aparece {conteo} veces.");
