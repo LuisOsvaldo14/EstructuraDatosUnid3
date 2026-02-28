22fb6d096661399cb0b869cf798ce15d82c88bcc
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 23:10:49 -0400

    Ejercicio 14

---
+++ APLI14/APLI14/Program.cs
@@ -0,0 +1,4 @@
+﻿var frutas = ("Manzana", "Pera", "Uva");
+var verduras = ("Zanahoria", "Tomate", "Lechuga");
+var mercado = (frutas, verduras);
+Console.WriteLine($"Unión: {mercado.frutas}, {mercado.verduras}");
