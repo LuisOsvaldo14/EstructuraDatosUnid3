fd362d4b5efe6d025bb1b9e4d7620d4d82fb0db2
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:56:30 -0400

    Ejercicio11

---
+++ APLI11/APLI11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Coche
+{
+    public string color;
+
+    public string ObtenerColor()
+    {
+        return color;
+    }
+
+    static void Main()
+    {
+        Coche miCoche = new Coche();
+        miCoche.color = "Rojo";
+        Console.WriteLine($"El color del coche es: {miCoche.ObtenerColor()}");
+    }
+}
