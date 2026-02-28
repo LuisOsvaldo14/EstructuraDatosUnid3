42f448e8771f062306cdd4a000310e2b5434307c
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:14:47 -0400

    Ejercicio 2

---
+++ APLI2/APLI2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class AreaCirculo
+{
+    static void Main()
+    {
+        Console.Write("Introduce el radio del círculo: ");
+        double r = double.Parse(Console.ReadLine());
+        Console.WriteLine($"El área es: {CalcularArea(r):F2}");
+    }
+
+    static double CalcularArea(double radio) => Math.PI * Math.Pow(radio, 2);
+}
