1d25d8850dc2332a29a666f8865aabcfb2fb13f9
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:27:42 -0400

    Ejercicio 5

---
+++ APLI5/APLI5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Vocales
+{
+    static void Main()
+    {
+        Console.Write("Escribe una frase: ");
+        string texto = Console.ReadLine();
+        Console.WriteLine($"Número de vocales: {ContarVocales(texto)}");
+    }
+
+    static int ContarVocales(string cadena)
+    {
+        string vocales = "aeiouAEIOUáéíóúÁÉÍÓÚ";
+        return cadena.Count(c => vocales.Contains(c));
+    }
+}
