335f983a7c045a6725779895872e3072f71f497e
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:51:03 -0400

    ejercicio10

---
+++ APLI10/APLI10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Perro
+{
+    public string raza;
+
+    public void ImprimirRaza()
+    {
+        Console.WriteLine($"La raza del perro es: {raza}");
+    }
+
+    static void Main()
+    {
+        Perro miPerro = new Perro();
+
+        miPerro.raza = "Frenchie";
+        miPerro.ImprimirRaza();
+    }
+}
