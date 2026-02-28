f62a0bda099c2618bf66875b8a8f38fea67e4a6f
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 23:02:47 -0400

    Ejercicio12

---
+++ APLI12/APLI12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Libro
+{
+    public string titulo;
+
+    public void ImprimirTitulo()
+    {
+        Console.WriteLine($"Título del libro: {titulo}");
+    }
+
+    static void Main()
+    {
+        Libro miLibro = new Libro();
+        miLibro.titulo = "Cien años de soledad";
+        miLibro.ImprimirTitulo();
+    }
+}
