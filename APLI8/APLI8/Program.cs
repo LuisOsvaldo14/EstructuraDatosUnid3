019aca4f742adf8ec0a3fcdb6df2623d22658e30
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:43:12 -0400

    Ejercicio8

---
+++ APLI8/APLI8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Longitud
+{
+    static void Main()
+    {
+        Console.Write("Escribe algo: ");
+        string entrada = Console.ReadLine();
+        Console.WriteLine($"La longitud es de {CalcularLargo(entrada)} caracteres.");
+    }
+
+    static int CalcularLargo(string texto) => texto.Length;
+}
