093bfe59b58f444f697f10cf279e514de4f9fbba
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:18:17 -0400

    Ejercicio 3

---
+++ APLI3/APLI3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class VerificarPar
+{
+    static void Main()
+    {
+        Console.Write("Introduce un número: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (EsPar(num)) Console.WriteLine("El número es Par.");
+        else Console.WriteLine("El número es Impar.");
+    }
+
+    static bool EsPar(int n) => n % 2 == 0;
+}
