190e3b47bffe0447341372bfed40a55cf4d3cc29
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:48:04 -0400

    Ejercicio 9

---
+++ APLI 9/APLI 9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Persona
+{
+    public string nombre; 
+
+    public void Saludar()
+    { 
+        Console.WriteLine($"Hola, soy {nombre}");
+    }
+
+    static void Main()
+    {
+        Persona p = new Persona();
+        Console.Write("Ingresa tu nombre: ");
+        p.nombre = "Rodorfo";
+        p.Saludar();
+    }
+}
