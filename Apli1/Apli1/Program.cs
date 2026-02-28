4a9ef9c6931abc4303d7438f0d73602bbde5f726
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-27 22:11:14 -0400

    EJercicio 1 Terminado

---
+++ Apli1/Apli1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Suma
+{
+    static void Main()
+    {
+        Console.Write("Introduce el primer número: ");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.Write("Introduce el segundo número: ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Resultado de la suma: {Sumar(n1, n2)}");
+    }
+
+    static int Sumar(int a, int b) => a + b;
+}
